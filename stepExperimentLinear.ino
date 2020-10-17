ef34adca3c2cf54ef517c0e545e411ba462c7ba6
madisonheeg <70350500+madisonheeg@users.noreply.github.com>
2020-10-17 15:50:45 -0500

    Oct 17 2:50PM

--- stepExperimentLinear.ino
+++ stepExperimentLinear.ino
@@ -16,13 +16,13 @@
 
 /**************************************** PIN DEFINITIONS ****************************************/ 
 /* RIGHT MOTOR - Motor B */
-#define R_PWM           9            // PWM (black)
+#define R_PWM           10            // PWM (black)
 #define R_SIGN          8            // direction (red)
 #define A_RW            2            // channel A, ISR (yellow)  
 #define B_RW            6            // channel B (white)
 
 /* LEFT MOTOR - Motor A */
-#define L_PWM           10           // PWM (black)
+#define L_PWM           9           // PWM (black)
 #define L_SIGN          7            // direction (red)
 #define A_LW            3            // channel A, ISR (orange)
 #define B_LW            11           // channel B (white)
@@ -52,9 +52,9 @@ double        R_angPosNow = 0.0;        // [rad]      current angular position
 double        R_angVelNow = 0.0;        // [rad/s]    angular velocity
 double        R_angVelPrev = 0.0;       // [rad/s]    angular velocity
 double        R_linVel = 0.0;           // [m/s]      linear velocity
-unsigned long R_timePrev = 0;           // [s]        prev time
-unsigned long R_timeNow = 0;            // [s]        current time
-unsigned long R_deltaT = 0;             // [s]        difference of current - prev time
+unsigned long R_timePrev = 0;           // [us]        prev time
+unsigned long R_timeNow = 0;            // [us]        current time
+unsigned long R_deltaT = 0;             // [us]        difference of current - prev time
 
 
 
@@ -69,9 +69,9 @@ double        L_angPosNow = 0.0;        // [rad]      current angular position
 double        L_angVelNow = 0.0;        // [rad/s]    angular velocity
 double        L_angVelPrev = 0.0;       // [rad/s]    angular velocity
 double        L_linVel = 0.0;           // [m/s]      linear velocity
-unsigned long L_timePrev = 0;           // [s]        prev time
-unsigned long L_timeNow = 0;            // [s]        current time
-unsigned long L_deltaT = 0;             // [s]        difference of current - prev time
+unsigned long L_timePrev = 0;           // [us]        prev time
+unsigned long L_timeNow = 0;            // [us]        current time
+unsigned long L_deltaT = 0;             // [us]        difference of current - prev time
 
 
 /* ROBOT */
@@ -82,13 +82,16 @@ unsigned long L_deltaT = 0;             // [s]        difference of current - pr
  *  1 0 CW
  *  1 1 B
 */
-const unsigned long  tooQuick =0.001;                                     // [s]   1 ms range if enter ISR too quickly
-bool          enterISR = true;                                            // flag to detect if motor variables changed
-double        x_prev = 0.0; double y_prev = 0.0; double phi_prev = 0.0;   // [in]    prev positions
-double        x_now = 0.0; double y_now = 0.0; double phi_now = 0.0;      // [in]    current positions
-unsigned long timePrev = 0;                                               // [s]    time exit main loop
-unsigned long timeNow = 0;                                                // [s]    time enter main loop
-unsigned long deltaT = 0;                                                 // [s]    time diff b/t exit and enter   
+const unsigned long  tooQuick = 10;                                           // [us]   1 ms range if enter ISR too quickly
+bool          enterISR = true;                                                // flag to detect if motor variables changed
+double        x_prev = 0.0; double y_prev = 0.0; double phi_prev = 0.0;       // [in]   starting positions
+double        x_now = 0.0; double y_now = 0.0; double phi_now = 0.0;          // [in]   current positions
+unsigned long timePrev = 0;                                                   // [s]    time exit main loop
+unsigned long timeNow = 0;                                                    // [s]    time enter main loop
+unsigned long deltaT = 0;                                                     // [s]    time diff b/t exit and enter
+
+//double x_posCalib = 4.16;                                                    [in] calibration for front of camera to be 0 origin
+//double y_posCalib = 4.16;                                                    [in] calibration for front of camera to be 0 origin 
 
 bool          R_dir = 0;                // controls the direction of rotation
 bool          L_dir = 0;                // controls the direction of rotation
@@ -147,21 +150,20 @@ void loop() {
   if(enterISR) {          /* update x, y, phi of robot */
     calculatePosition(); 
       /* Print statements */
-      //Serial.print("RW: "); Serial.print(R_countNow); Serial.print("\t"); Serial.print("LW: "); Serial.print(L_countNow);
-      //Serial.print("\t"); Serial.print("X: "); Serial.println(x_now); //Serial.print("\t"); Serial.print(y_now); Serial.print("\t"); Serial.println(phi_now); 
+      //Serial.print("RW: "); Serial.print(R_countNow); Serial.print("\t"); Serial.print("LW: "); Serial.print(L_countNow); Serial.print("\t");
+     //Serial.print(x_now); Serial.print("\t"); Serial.print(y_now); Serial.print("\t"); Serial.println(phi_now); 
   }
   
   if(updateRW) {          /* update angPos, angVel, and linVel of right wheel */
-    //Serial.print("R count: "); Serial.println(R_countNow);
     updateRightWheel();
   }
   
   if(updateLW) {          /* update angPos, angVel, and linVel of left wheel */
     updateLeftWheel();
-    //Serial.print("L count: "); Serial.println(L_countNow);
   }
 
   if((millis()%50 == 0) && (millis() <= 2800)){   /* sample velocity of both left and right wheels every 50 ms while time <= 2.8 s */
+    //Serial.print(L_angPosNow); Serial.print("\t"); Serial.print(L_deltaT); Serial.print("\t"); Serial.print(R_angPosNow); Serial.print("\t"); Serial.print(R_deltaT); Serial.print("\t"); 
     Serial.print((double)millis()/(double)1000);  Serial.print("\t"); Serial.print(L_linVel); Serial.print("\t"); Serial.println(R_linVel);
   }
 
@@ -196,7 +198,7 @@ void updateLW_countsISR() {
 
 /* update right wheel velocities and positions */
 void updateRightWheel() {
-    R_timeNow = micros()/(unsigned long)micro;      // capture the time enter ISR
+    R_timeNow = micros();      // capture the time enter ISR
     R_deltaT = R_timeNow - R_timePrev;              // calculate time since last ISR
 
     if (R_deltaT > tooQuick){
@@ -207,7 +209,7 @@ void updateRightWheel() {
                     R_countNow = 0; R_angPosNow = 0.0;                      // reset characteristics to zero
                     R_angVelNow = R_angVelPrev;                             // current angVel set to previous angVel
                 } else {
-                  R_angVelNow = (R_angPosNow - R_angPosPrev)/((double)R_deltaT);  // else, calculate angVel based on angPos
+                  R_angVelNow = (R_angPosNow - R_angPosPrev)/((double)R_deltaT/(double)micro);  // else, calculate angVel based on angPos
                 }
                 
                 R_linVel = radius*R_angVelNow;              // calculate linear velocity
@@ -215,7 +217,7 @@ void updateRightWheel() {
                 updateRW = false;
                 R_angPosPrev = R_angPosNow;             
                 R_angVelPrev = R_angVelNow; 
-                R_timePrev = micros()/(unsigned long)micro; // capture time leaving ISR
+                R_timePrev = micros(); // capture time leaving ISR
           }
     }
 }
@@ -223,7 +225,7 @@ void updateRightWheel() {
 
 /* update left wheel velocities and positions */
 void updateLeftWheel() {
-    L_timeNow = micros()/(unsigned long)micro;      // capture the time enter ISR
+    L_timeNow = micros();      // capture the time enter ISR
     L_deltaT = L_timeNow - L_timePrev;              // calculate time since last ISR
 
     if (L_deltaT > tooQuick){
@@ -234,7 +236,7 @@ void updateLeftWheel() {
                     L_countNow = 0; L_angPosNow = 0.0;                       // reset characteristics to zero
                     L_angVelNow = L_angVelPrev;                             // current angVel set to previous angVel
                 } else {
-                  L_angVelNow = (L_angPosNow - L_angPosPrev)/((double)L_deltaT);  // else, calculate angVel based on angPos
+                  L_angVelNow = (L_angPosNow - L_angPosPrev)/((double)L_deltaT/(double)micro);  // else, calculate angVel based on angPos
                 }
                 
                 L_linVel = radius*L_angVelNow;              // calculate linear velocity
@@ -242,26 +244,26 @@ void updateLeftWheel() {
                 updateLW = false;
                 L_angPosPrev = L_angPosNow;             
                 L_angVelPrev = L_angVelNow; 
-                L_timePrev = micros()/(unsigned long)micro; // capture time leaving ISR
+                L_timePrev = micros(); // captre time leaving ISR
           }
     }
 }
 
 /* Calculate new position and angle of robot*/
 void calculatePosition() {
-  timeNow = micros()/(unsigned long)micro;        // capture time enter loop
+  timeNow = micros();        // capture time enter loop
   deltaT = timeNow - timePrev;                    // calculate time since last enter
   
   /* calculate position and angle */
-  x_now = x_prev + ((double)deltaT*double(cos(phi_prev))*(R_linVel + L_linVel))/(2.0);
-  y_now = y_prev + ((double)deltaT*double(sin(phi_prev))*(R_linVel + L_linVel))/(2.0);
-  phi_now = phi_prev + (double)deltaT*(radius/baseline)*(R_linVel-L_linVel);
+  x_now = x_prev + (((double)deltaT/(double)micro)*double(cos(phi_prev))*(R_linVel + L_linVel))/(2.0);
+  y_now = y_prev + (((double)deltaT/(double)micro)*double(sin(phi_prev))*(R_linVel + L_linVel))/(2.0);
+  phi_now = phi_prev + ((double)deltaT/(double)micro)*(radius/baseline)*(R_linVel-L_linVel);
            
   /* set previous values to current values */
   x_prev = x_now;
   y_prev = y_now;
   phi_prev = phi_now;
   enterISR = false;
-  timePrev = micros()/(double)micro;   
+  timePrev = micros();   
   
 }
