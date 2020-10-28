bf772a06bd342dc305c07216bf48d6711841a154
castellano-m <70445904+castellano-m@users.noreply.github.com>
2020-10-28 12:33:06 -0600

    Update MarkerDetect+AngleCalc.py

--- MarkerDetect+AngleCalc.py
+++ MarkerDetect+AngleCalc.py
@@ -3,6 +3,7 @@
 # Demo 1
 # Marker detection and angle calculation
 
+# Initialize Libraries
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 from time import sleep
@@ -15,18 +16,21 @@ import cv2
 import PIL
 import math
 
-# Gloval Variables
+# Global Variables
 global quadrant, aruco_dict, parameters, frame, corners, ids, rejectedImgPoints, frame_markers
 # Turn on video
 video = cv2.VideoCapture(0)
-        
+
+# Calculates angle from center of camera
 def aruco_location():
     # Adjust fov
-    fov = .12 *1280*7 #3.05mm but in inches
-    real_distance = 6.0 /1.8# 5.5 inch
+    fov = .12 *1280*7                   #3.05mm but in inches
+    real_distance = 6.0 /1.8            # 5.5 inch
     x_distance = (corners[0][0][1][0]- corners[0][0][0][0]) ** 2
     y_distance = (corners[0][0][1][1] - corners[0][0][0][1]) ** 2
     image_distance = corners[0][0][1][0] - corners[0][0][0][0]
+    
+    # Finds the distance from the center camera to the center of the aruco marker
     distance = fov * real_distance / image_distance
     
     reference1 = math.sqrt((corners[0][0][3][0] - corners[0][0][0][0])**2 +
@@ -36,21 +40,21 @@ def aruco_location():
                            (corners[0][0][2][1] - corners[0][0][1][1])**2) + corners[0][0][2][0]
                            
     imageCenter = (reference1 / 2) + (reference2 / 2) / 2
+    
     # Calculate center of marker based on four corners
     x_cord = ((((corners[0][0][1][0] - corners[0][0][0][0])/2)
                + ((corners[0][0][3][0] - corners[0][0][2][0])/2)) / 2) + corners[0][0][0][0]
     
     y_cord = ((((corners[0][0][1][1] - corners[0][0][0][1])/2)
                + ((corners[0][0][3][1] - corners[0][0][2][1])/2)) / 2) + corners[0][0][0][1]
+    
+    # Having center of marker in inches, calculate corresponding angle
     center = ((1280/2) - (x_cord-300 * -1))
-    print(center)
     X = center / (distance *128)
-    print()
-    print(X)
+    
     # Measurers angle from [30,0] from left side to right side of view
     angle = math.asin(X)*1000
-    # Convert angle value between [-15,15]
-    #print("Real Distance: ", distance)
+    print("Real Distance: ", distance)
     print("Angle: " , angle)
 
     
